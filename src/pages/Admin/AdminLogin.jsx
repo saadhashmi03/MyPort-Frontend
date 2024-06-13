c1f7aa43b0d438c49ed2e802183322fb495781c6
saadhashmi03 <116589433+saadhashmi03@users.noreply.github.com>
2024-06-13 18:59:14 +0530

    fix admin login

--- src/pages/Admin/AdminLogin.jsx
+++ src/pages/Admin/AdminLogin.jsx
@@ -14,7 +14,7 @@ const AdminLogin = () => {
 
   const loginUser = async (e) => {
     e.preventDefault();
-    const res = await axios.post("https://localhost:5000/api/login", {
+    const res = await axios.post("http://localhost:5000/api/login", {
       email,
       password,
     });
@@ -28,7 +28,7 @@ const AdminLogin = () => {
 
 
   const checkUser = async()=>{
-    const res = await  axios.get("https://localhost:5000/api/checkUser",{
+    const res = await  axios.get("http://localhost:5000/api/checkUser",{
       withCredentials:true,
     });
     const data = await res.data;
